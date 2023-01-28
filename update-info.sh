b68a410b9e1018bd2dfceb343f1bc5cc4020f757
stwe <no@mail.tt>
2023-01-28 15:05:12 +0100

    add comment

--- update-info.sh
+++ update-info.sh
@@ -1,2 +1,5 @@
+# add the following line in your sudoers
+# username ALL=(ALL) NOPASSWD:/usr/bin/apt update
+
 readonly U=$(sudo apt update 2>/dev/null)
 LANG=C apt-get dist-upgrade -s |grep -P '^\d+ upgraded'|cut -d" " -f1
