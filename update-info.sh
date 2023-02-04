482cabc3297b895691449607cf63890d8900be24
stwe <no@mail.tt>
2023-02-04 23:27:51 +0100

    last time

--- update-info.sh
+++ update-info.sh
@@ -3,8 +3,10 @@
 
 # be sure, the Tango Theme is available in /usr/share/icons
 
+LAST=$(date +%T)
+
 readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-readonly U=$(sudo apt update 2>/dev/null)
+#readonly U=$(sudo apt update 2>/dev/null)
 readonly O=$(LANG=C apt-get dist-upgrade -s |grep -P '^\d+ upgraded'|cut -d" " -f1)
 
 if [ "$O" = "0" ]; then
@@ -14,3 +16,6 @@ else
     AVAIL="<img>/usr/share/icons/Tango/16x16/status/software-update-urgent.png</img>"
     echo -e "${AVAIL}<txt> $O updates available.</txt><click>exo-open --launch TerminalEmulator sudo apt dist-upgrade</click>"
 fi
+
+MORE_INFO="<tool>Last time checked for updates: $LAST</tool>"
+echo -e "${MORE_INFO}"
