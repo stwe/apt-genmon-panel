57b9531bae2a1d686c9ccab58d158058938c7133
stwe <no@mail.tt>
2023-02-04 21:55:38 +0100

    add icons

--- update-info.sh
+++ update-info.sh
@@ -1,5 +1,15 @@
 # add the following line in your sudoers
 # username ALL=(ALL) NOPASSWD:/usr/bin/apt update
 
+# be sure, the Tango Theme is available in /usr/share/icons
+
 readonly U=$(sudo apt update 2>/dev/null)
-LANG=C apt-get dist-upgrade -s |grep -P '^\d+ upgraded'|cut -d" " -f1
+readonly O=$(LANG=C apt-get dist-upgrade -s |grep -P '^\d+ upgraded'|cut -d" " -f1)
+
+if [ "$O" = "0" ]; then
+    Z0="<img>/usr/share/icons/Tango/16x16/apps/system-software-update.png</img>"
+    echo -e "${Z0}<txt> Your system is up to date.</txt>"
+else
+    AVAIL="<img>/usr/share/icons/Tango/16x16/status/software-update-urgent.png</img>"
+    echo -e "${AVAIL}<txt> $O updates available.</txt>"
+fi
