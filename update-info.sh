bc422a9daafc4f2388bad9530047b161f5c8228b
stwe <no@mail.tt>
2023-02-10 22:55:03 +0100

    feat: add list of updatable packages

--- update-info.sh
+++ update-info.sh
@@ -5,7 +5,7 @@
 
 # be sure, the Tango Theme is available in /usr/share/icons
 
-readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly last=$(date +%T)
 readonly u=$(sudo apt update 2>/dev/null)
 readonly updates=$(LANG=C apt-get dist-upgrade -s |grep -P '^\d+ upgraded'|cut -d" " -f1)
@@ -18,8 +18,9 @@ if [ "$updates" = "0" ]; then
     echo "<click>xfce4-panel --plugin-event=genmon-13:refresh:bool:true</click>"
 else
     # system needs to be updated
-    echo "<txt>$updates updates available at $last.</txt>"
-    echo -e "<tool><span foreground='#FF0000'>Click on the icon to update the system.</span></tool>"
+    readonly l=$(apt list --upgradable | column -t | awk '{print "\\n",$1"\t"$2"\t"$6}' | column -t | tr -d "]")
+    echo "<txt>$updates updates available. Click on icon to update.</txt>"
+    echo -e "<tool>"$l"</tool>"
     echo "<img>/usr/share/icons/Tango/16x16/status/software-update-urgent.png</img>"
-    echo "<click>${DIR}/upgrade.sh</click>"
+    echo "<click>${dir}/upgrade.sh</click>"
 fi
